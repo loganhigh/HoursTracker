3a212ce86bfdb6166a248e6ce290d56cfd4afafe
Logan <loganh1571@gmail.com>
2026-01-21 01:49:17 -0700

    Initial Commit

---
+++ HoursTracker/HoursTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoursTrackerApp.swift
+//  HoursTracker
+//
+//  Created by logan high on 2026-01-21.
+//
+
+import SwiftUI
+
+@main
+struct HoursTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
